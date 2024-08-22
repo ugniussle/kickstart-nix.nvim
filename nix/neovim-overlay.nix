7918c4a3769be01e57c8ee9bd7376dab7897c412
ugniussle <ugniussle@gmail.com>
2024-08-22 07:18:57 +0300

    update

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -29,6 +29,8 @@ with final.pkgs.lib; let
     # plugins from nixpkgs go in here.
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
     nvim-treesitter.withAllGrammars
+    nvim-treesitter-parsers.javascript
+    nvim-treesitter-parsers.css
     nvim-treesitter-parsers.typescript
     nvim-treesitter-parsers.tsx
     nvim-treesitter-parsers.html
